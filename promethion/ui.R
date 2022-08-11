2a25dbe14992ab1de3f11ea651fc3de52085acfe
Becky Moody <moody.reb@gmail.com>
2022-08-11 11:37:30 -0500

    moved plotting tab server and ui to separate files

--- promethion/ui.R
+++ promethion/ui.R
@@ -41,80 +41,8 @@ tagList(
   navbarPage('Promethion', id = 'promethion_app',
 
              source(here::here('tabs/data_editor_ui.R'), local = TRUE)$value,
+             source(here::here('tabs/plotting_ui.R'), local = TRUE)$value,
 
-tabPanel('Plot Data',
-         sidebarPanel(width = 3,
-####################################################-
-                      # show correct header if there is data or not
-                      div(id='no_data_warning',h2('Please upload data.')),
-                      div(id='filter_tab_name',h2('Apply Filters')),
-####################################################-
-                      # choose column with subject ids
-                      shinyWidgets::pickerInput(
-                        inputId = 'which_column_subject_id',
-                        label = 'Which column in data contains subject ids?',
-                         choices =  character(0),
-                          selected = character(0),
-                          options = list(`actions-box` = TRUE,
-                                         `live-search`=TRUE),
-                          multiple = FALSE
-                        ),
-####################################################-
-                      div(id = 'all_plot_filters',
-          tags$hr(style="border-color: black;"),
-          shinyWidgets::pickerInput(inputId = 'plot_animal_filter',
-                                    label = HTML(paste(
-                                      h4('Which subject(s)?',style = "display: inline;"))),
-                                    choices =  character(0),
-                                    selected = character(0),
-                                    options = list(
-                                      `actions-box` = TRUE,
-                                      `live-search`=TRUE),
-                                    multiple = TRUE
-          ),
-
-          ## this update dropdown is in data_editor_server.R
-          shinyWidgets::pickerInput(inputId = 'plot_metric_filter',
-                                    label = HTML(paste(
-                                      h4('Which metric?',style = "display: inline;"),
-                                      h6(em("'_diff' metrics are the change between time."),style = "display: inline;"))),
-                                    choices =  character(0),
-                                    selected = character(0),
-                                    options = list(
-                                      `actions-box` = TRUE,
-                                      `live-search`=TRUE),
-                                    multiple = FALSE
-          ),
-
-          shinyWidgets::pickerInput(inputId = 'plot_phase_filter',
-                                    label = HTML(paste(
-                                      h4('Which light/dark phase?',style = "display: inline;"))),
-                                    choices =  character(0),
-                                    selected = character(0),
-                                    options = list(
-                                      `actions-box` = TRUE,
-                                      `live-search`=TRUE),
-                                    multiple = TRUE
-          ),
-          tags$hr(style="border-color: black;")
-          ),
-####################################################-
-          actionButton(
-            inputId = "plot_selected",
-            label = "Plot selected", width = '100%'),
-        shinyWidgets::progressBar(id = "plot_filter_progress", value = 0, striped = TRUE),
-          br()
-), # end sidebarpanel
-
-
-####################################################-
-          mainPanel(width = 9,
-                    plotOutput('filtered_prom_plot')
-                    #plotOutput("distPlot")
-          )
-
-
-         ),# end tabpanel
 
 ####################################################-
 tabPanel('Example',
---
+++ tabs/plotting_server.R
@@ -0,0 +1,219 @@
+## only show anything if you click on this tab
+observeEvent(input$promethion_app == 'Plot Data',{
+
+  ## hide/show sidebar items
+  if(is.null(final_df())){
+
+    message('Need data uploaded.')
+    shinyjs::show(id='no_data_warning')
+
+    shinyjs::hide(id='filter_tab_name')
+    shinyjs::hide(id="which_column_subject_id")
+
+    shinyjs::hide(id='all_plot_filters')
+    ## grouped these
+    # shinyjs::hide(id="plot_metric_filter")
+    # shinyjs::hide(id='plot_animal_filter')
+    # shinyjs::hide(id="plot_phase_filter")
+
+    shinyjs::hide(id="plot_selected")
+    shinyjs::hide(id="plot_filter_progress")
+
+  } else{
+
+    shinyjs::hide(id='no_data_warning')
+
+    shinyjs::show(id='filter_tab_name')
+    shinyjs::show(id="which_column_subject_id")
+    shinyjs::show(id='all_plot_filters')
+
+    shinyjs::show(id='plot_selected')
+    shinyjs::hide(id="plot_filter_progress")
+  }
+
+  req(!is.null(final_df()))
+  final_df_cols <- colnames(final_df())
+  ## if there are column names (could change this to if final_df is null)
+  #if(!is.null(final_df_cols)){
+
+  ## show and update phase filter if the light_dark col has been calculated
+  if('light_dark' %in% final_df_cols){
+    shinyjs::show(id='plot_phase_filter')
+    final_df_phases <- unique(final_df()$phase)
+    shinyWidgets::updatePickerInput(session,
+                                    "plot_phase_filter",
+                                    choices = c(final_df_phases),
+                                    selected = c(final_df_phases))
+  }
+
+  ## update column selections for subject ids
+  shinyWidgets::updatePickerInput(session,
+                                  "which_column_subject_id",
+                                  # label = "download_data_col",
+                                  choices = c(final_df_cols),
+                                  selected = character(0))
+
+  ## show and update subject id selections after choosing col
+  observeEvent(input$which_column_subject_id,{
+    shinyjs::show(id="plot_animal_filter")
+    # create a new column that is subject ids; assuming this column name isn't there
+    final_df(final_df() %>% mutate(study_subject_id = !!rlang::sym(input$which_column_subject_id)))#final_df()[input$which_column_subject_id]))
+    sub_ids <- unique(final_df()$study_subject_id) #unique(final_df()[input$which_column_subject_id])
+    shinyWidgets::updatePickerInput(session,
+                                    "plot_animal_filter",
+                                    # label = "download_data_col",
+                                    choices = c(sub_ids),
+                                    selected = character(0))
+  })
+
+  metrics <- unique(final_df()$metric)
+  shinyWidgets::updatePickerInput(session,
+                                  "plot_metric_filter",
+                                  # label = "download_data_col",
+                                  choices = c(metrics),
+                                  selected = character(0))
+
+
+  observeEvent(input$plot_selected,{
+    df <- data.frame()
+    # going to nest if this
+    if(input$plot_selected > 0 & !is.null(input$plot_selected)){
+
+      shinyjs::show(id="plot_filter_progress")
+
+      message('Filtering data for plotting')
+      shinyWidgets::updateProgressBar(session, id = 'plot_filter_progress', value = 20)
+      df <- final_df() %>% mutate(date_time = as.POSIXct(date_time))
+      shinyWidgets::updateProgressBar(session, id = 'plot_filter_progress', value = 40)
+      Sys.sleep(1)
+      # only apply filters if these have values
+      if(!is.null(input$plot_animal_filter)){
+
+        message('Subjects: ', input$plot_animal_filter) # this doesn't have spaces but it's for me anyway
+        df <- df %>% filter(study_subject_id %in% input$plot_animal_filter)
+        #!!rlang::sym(input$which_column_subject_id) %in% input$plot_animal_filter)
+      } else { # or just select cages, could be a lot if there are multiple runs
+        df <- df %>% filter(cage_num %in% c(1,2))
+      }
+
+      shinyWidgets::updateProgressBar(session, id = 'plot_filter_progress', value = 60)
+
+
+
+      if(!is.null(input$plot_metric_filter)){
+        message('Metrics: ', input$plot_metric_filter)
+
+        df <- df %>% filter(metric %in% input$plot_metric_filter)
+      } else{ # or just select one random metric
+        df <- df %>% filter(metric == sample(metrics,1))
+      }
+
+      # this is the only optional filter
+      if(!is.null(input$plot_phase_filter)){
+        message('Phases: ', input$plot_phase_filter)
+        df <- df %>% filter(phase %in% input$plot_phase_filter)
+      }
+
+      shinyWidgets::updateProgressBar(session, id = 'plot_filter_progress', value = 95)
+      Sys.sleep(1)
+      shinyjs::hide(id="plot_filter_progress")
+    }
+
+    plot_df(df)
+
+  }, ignoreNULL = FALSE)
+
+
+}, ignoreInit = TRUE)
+
+plot_gg_ob <- eventReactive(plot_df(),{
+  light_colors = c(light = "#FFF68F", dark = "#8DEEEE")
+  print(input$plot_phase_filter)
+
+  req(nrow(plot_df())>0)
+  message('Building plot')
+  if(!is.null(input$plot_phase_filter)){
+
+    message('Getting line breaks for plots')
+    # for line breaks of when light/dark switches
+    phase_changes <- plot_df() %>% group_by(metric) %>%
+      mutate(max_value = max(value),
+             min_value = min(value)) %>% ungroup() %>%
+      mutate(value_range = max_value - min_value,
+             text_placement = value_range * (2/3)+min_value) %>%
+      group_by(metric, phase_num, phase, max_value,text_placement) %>%
+      summarise(min_dt = min(date_time))
+
+    message('Getting background for plots')
+    # shade the background for light/dark
+    shaded_rect <- plot_df() %>% group_by(metric) %>%
+      mutate(y_max = max(value)) %>%
+      group_by(study_subject_id, metric, light_dark, phase_num, y_max) %>%
+      summarise(start_time = min(date_time), end_time = max(date_time),
+                y_min = min(value)) %>%
+      ungroup() %>% group_by(metric) %>%
+      mutate(y_min = ifelse(any(y_min==0),0,y_min))
+
+    message('Starting ggplot')
+    #plot
+    ggplot(plot_df())+ theme_minimal() +
+      # background light/dark
+      geom_rect(data = shaded_rect, aes(xmin = start_time,
+                                        xmax = end_time,
+                                        ymin = y_min,
+                                        ymax = y_max,
+                                        fill = light_dark),
+                alpha = .75, stat = 'identity')+
+      # set background colors
+      scale_fill_manual(name = 'Phase', values = light_colors) +
+      # lines for breaks
+      geom_vline(data = phase_changes, aes(xintercept = min_dt),
+                 show.legend = FALSE, color = 'black',linetype = 'dashed',alpha=.5 )+
+      # text for line breaks
+      geom_text(data = phase_changes, aes(x = min_dt,
+                                          y = text_placement,
+                                          label = phase),
+                # move label forward 1 hour (in seconds), rotate text, set font size, set transparency
+                nudge_x = 3600, angle = 90, size = 4, alpha = .5)+
+      # do points last so they're on top
+      geom_point(aes(date_time, value, color = study_subject_id)) +
+      # grid by each metric
+      facet_grid(~metric, scales = 'free', switch = 'y')
+
+    #shinipsum::random_ggplot(type = 'boxplot')
+    # p <- ggplot(plot_df()) +
+    #   theme_minimal()+
+    #   labs(#title = plot_title,
+    #     y = '',
+    #     x = 'Date time') +
+    # background colors for light phases
+    # geom_rect(data=shaded_rect, aes(xmin = start_time, xmax = end_time, ymin = y_min, ymax = y_max, fill = light_phase),
+    #           alpha = .75, stat = 'identity') +
+    # scale_fill_manual(name = 'Phase', values= light_colors)+
+    # vertical lines indicating change in light phase
+    # geom_vline(data=phase_changes, aes(xintercept = min_dt),
+    #            show.legend = FALSE, color = 'black', linetype = 'dashed', alpha = .5)+
+    # # label for the lines
+    # geom_text(data=phase_changes, aes(x = min_dt, y = max_y, label = phase_num),
+    #           # move label forward 1 hour (in seconds), rotate text, set font size, set transparency
+    #           nudge_x = 3600, angle = 90, size = 4, alpha = .5
+    # ) +
+    # do this last so points are on top
+    # geom_point(aes(date_time, value, color = subject_id))  +
+    # facet_grid(rows = metric, scales = 'free_y', switch = 'y')
+  } else{
+
+    message('No phases to shade')
+    message('Starting ggplot')
+    #plot
+    ggplot(plot_df())+ theme_minimal() +
+      # do points last so they're on top
+      geom_point(aes(date_time, value, color = study_subject_id)) +
+      # grid by each metric
+      facet_grid(~metric, scales = 'free', switch = 'y')
+
+  }
+
+}, ignoreInit = TRUE)
+
+output$filtered_prom_plot <- renderPlot(plot_gg_ob())#shinipsum::random_ggplot(type = 'bar'))
